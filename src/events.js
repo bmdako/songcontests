321d2b9de138a188df5d48d798c82a9f1ae77d76
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2015-02-04 22:52:15 +0100

    scores

--- src/events.js
+++ src/events.js
@@ -43,9 +43,10 @@ function selectEvent(ident, callback) {
     if (event === null) return callback (null, null);
 
     var sql = [
-      'SELECT songs.*, se.play_order',
+      'SELECT songs.*, se.play_order, likes.likes, likes.dislikes, likes.total',
       'FROM songs',
       'JOIN song_event se ON se.song_id = songs.id',
+      'LEFT JOIN (SELECT song_id, SUM(dislike=0) AS likes, SUM(dislike=1) AS dislikes, count(*) AS total FROM likes WHERE event_id = ' + mysql.escape(event.id) + ' GROUP BY event_id, song_id) AS likes ON likes.song_id = songs.id',
       'WHERE se.event_id = ' + mysql.escape(event.id),
       'ORDER BY se.play_order ASC'].join(' ');
 
@@ -57,13 +58,10 @@ function selectEvent(ident, callback) {
           event.active_song_index= index;
         }
 
-        var sql = [
-          'SELECT SUM(dislike=0) AS likes, SUM(dislike=1) AS dislikes, COUNT(*) AS total',
-          'FROM likes',
-          'WHERE event_id = ' + mysql.escape(event.id) + ' AND song_id = ' + mysql.escape(song.id),
-          'GROUP BY event_id, song_id'].join(' ');
-
-        mysql.queryOne(sql, console.log)
+        song.likes = song.likes === null ? 0 : song.likes;
+        song.dislikes = song.dislikes === null ? 0 : song.dislikes;
+        song.total = song.total === null ? 0 : song.total;
+        song.score = song.total === 0 ? 0 : parseInt((song.likes / song.total) * 100);
       });
 
       event.songs = songs;
