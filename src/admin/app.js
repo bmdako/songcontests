f48f3377b0d26ae0b75e01c68d0f4cc6fc655105
Tore Julø <toju@berlingskemedia.dk>
2015-02-05 15:52:40 +0100

    - Added button:disabled styling - bound events to like buttons - bound events to like scores

--- src/admin/app.js
+++ src/admin/app.js
@@ -32,19 +32,7 @@ songcontestsAdminApp.controller('EventsController', function ($scope, $resource,
 
 songcontestsAdminApp.controller('EventController', function ($scope, $resource, $http, $routeParams, socket) {
 
-
-
-  socket.forward('nowplaying', $scope);
-
-  $scope.$on('socket:nowplaying', function (ev, data) {
-    $scope.event.songs.forEach(function (el, idx, arr) {
-        if(el.id === data.song) {
-          el.nowplaying = 1;
-        } else {
-          el.nowplaying = 0;
-        }
-    });
-  });
+  socket.emit('join', 'mgp2015');
 
   var Events = $resource('/events/:ident', { ident: '@ident' });
 
@@ -57,4 +45,64 @@ songcontestsAdminApp.controller('EventController', function ($scope, $resource, 
   $scope.setNowPlaying = function (song_id) {
     $http.post('/' + $routeParams.ident, {song: song_id, event: $routeParams.ident});
   };
+
+
+  $scope.like = function(song_id) {
+    console.log('like ' + song_id);
+    var obj =
+    socket.emit('like', {
+      song: song_id,
+      event: $scope.event.ident,
+      session: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10)
+    });
+  };
+
+  $scope.dislike = function(song_id) {
+    console.log('dislike ' + song_id);
+    socket.emit('dislike', {
+      song: song_id,
+      event: $scope.event.ident,
+      session: Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10)
+    });
+  };
+
+  /* SOCKET BINDINGS */
+
+  socket.forward('nowplaying', $scope);
+
+  $scope.$on('socket:nowplaying', function (ev, data) {
+    console.log(data);
+    $scope.event.songs.forEach(function (el, idx, arr) {
+      if(el.id === data.song) {
+        el.nowplaying = 1;
+        el.active     = 1;
+      } else {
+        el.nowplaying = 0;
+        el.active     = 0;
+      }
+    });
+  });
+
+  socket.forward('newrating', $scope);
+
+  $scope.$on('socket:newrating', function (ev, data) {
+    if(data.event === $scope.event.ident) {
+      $scope.event.songs.forEach(function (el, idx, arr) {
+        if(el.id === data.song) {
+          el.likes = data.likes;
+          el.dislikes = data.dislikes;
+          el.score = data.score;
+          el.total = data.total;
+          return true;
+        }
+      });
+    }
+  });
+
+  /* Leaving */
+
+  $scope.$on("$destroy", function(){
+    socket.emit('leave', 'mgp2015');
+  });
+
 });
