06ae60afb88a07fdae95e5093bc1bc5e6aca8794
Daniel Jozie Kokott <dako@berlingskemedia.dk>
2015-02-05 12:04:24 +0100

    NOW playing

--- src/admin/app.js
+++ src/admin/app.js
@@ -1,5 +1,22 @@
 var songcontestsAdminApp = angular.module('songcontestsAdminApp', ['ngRoute', 'ngResource']);
 
+songcontestsAdminApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/', {
+        templateUrl: 'partials/events-overview.html',
+        controller: 'EventsController'
+      }).
+      when('/:ident', {
+        templateUrl: 'partials/event-details.html',
+        controller: 'EventController'
+      }).
+      otherwise({
+        redirectTo: '/'
+      });
+  }]);
+
+
 songcontestsAdminApp.controller('EventsController', function ($scope, $resource, $http) {
   var Events = $resource('/events/:ident', { ident: '@ident' });
 
@@ -8,4 +25,18 @@ songcontestsAdminApp.controller('EventsController', function ($scope, $resource,
   };
 
   $scope.queryEvents();
+});
+
+songcontestsAdminApp.controller('EventController', function ($scope, $resource, $http, $routeParams) {
+  var Events = $resource('/events/:ident', { ident: '@ident' });
+
+  $scope.getEvent = function () {
+    $scope.event = Events.get({ ident: $routeParams.ident });
+  };
+
+  $scope.getEvent();
+
+  $scope.setNowPlaying = function (song_id) {
+    $http.post('/' + $routeParams.ident, {song: song_id, event: $routeParams.ident});
+  };
 });
